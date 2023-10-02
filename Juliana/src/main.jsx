0f7f1766b2eefa1211244e7ad4a87d7980887f96
Juliana Elias <eliasjuliana@gmail.com>
2023-10-02 19:16:35 -0300

    realizo ejercicio 7

--- Juliana/src/main.jsx
+++ Juliana/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 
 // import Ejercicio1 from './pages/Ejercicio1.jsx'
 // import Ejercicio2 from './pages/Ejercicio2.jsx'
-import Ejercicio3 from './pages/Ejercicio3'
+// import Ejercicio3 from './pages/Ejercicio3.jsx'
+import List from './pages/Ejercicio7.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './index.css'
+// import EmployeeCard from './Components/Ejercicio7/EmployeeCard'
 
 
 
@@ -15,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Ejercicio1 /> */}
     {/* <Ejercicio2 message='my friend'/> */}
-    <Ejercicio3 propsMessage='my friend' />
+    {/* <Ejercicio3 propsMessage='my friend' /> */}
+    <List/>
   </React.StrictMode>,
 )
---
+++ Juliana/src/pages/Ejercicio7.jsx
@@ -0,0 +1,31 @@
+import EmployeeCard from "../Components/Ejercicio7/EmployeeCard";
+
+
+// lista de empleados
+
+let employees = [
+    { id: 1, fullName: "Laya Dueñas", title: "CEO", department: "Business", pic: "https://api.dicebear.com/7.x/miniavs/svg?seed=Cali" },
+    { id: 2, fullName: "Astryd Vallés", title: "CMO", department: "Marketing", pic: "https://api.dicebear.com/7.x/miniavs/svg?seed=Baby" },
+    { id: 3, fullName: "Shantell Meza", title: "CFO", department: "Business", pic: "https://api.dicebear.com/7.x/miniavs/svg?seed=Charlie" },
+    { id: 4, fullName: "Sergio Ocampo", title: "CTO", department: "Engineering", pic: "https://api.dicebear.com/7.x/miniavs/svg?seed=Cookie" },
+    { id: 5, fullName: "Ares Jiménez", title: "Art Director", department: "Marketing", pic: "https://api.dicebear.com/7.x/miniavs/svg?seed=Leo" },
+    { id: 6, fullName: "Marta Pérez", title: "Frontend Dev", department: "Engineering", pic: "https://api.dicebear.com/7.x/miniavs/svg?seed=Tigger" },
+    { id: 7, fullName: "Ellen Balderas", title: "Digital Strategist", department: "Marketing", pic: "https://api.dicebear.com/7.x/miniavs/svg?seed=Peanut" },
+    { id: 8, fullName: "Cynthia Valentín", title: "Backend Dev", department: "Engineering", pic: "https://api.dicebear.com/7.x/miniavs/svg?seed=Lily" },
+    { id: 9, fullName: "Bernard Jung", title: "DevOps Engineer", department: "Engineering", pic: "https://api.dicebear.com/7.x/miniavs/svg?seed=Pepper" },
+  ]
+  
+
+const List = () => {
+  return (
+    <section className="container p-3">
+    {
+        employees.map((item)=>{
+            return <EmployeeCard key={item.id} fullName = {item.fullName} image = {item.pic} title = {item.title} department = {item.department}/>
+        })
+    }
+</section>
+  )
+}
+
+export default List
